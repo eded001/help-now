d9ec3bfb9f4757dd9605ae99e9ebceb91a037026
Ed <108343328+eded001@users.noreply.github.com>
2025-07-20 13:33:25 -0300

    feat: configurações de ambiente do projeto

---
+++ generate-env.js
@@ -0,0 +1,52 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const interfaces = os.networkInterfaces();
+let ip = null;
+
+const PORTS = {
+    HOST_PORT: process.argv[0] || 5510,
+    HOST_WEBSOCKET_PORT: process.argv[1] || 5515,
+    CLIENT_PORT: process.argv[2] || 5520
+};
+
+for (const name in interfaces) {
+    for (const iface of interfaces[name]) {
+        if (iface.family === 'IPv4' && !iface.internal) {
+            ip = iface.address;
+            break;
+        }
+    }
+    if (ip) break;
+}
+
+if (!ip) {
+    console.error('Não foi possível obter o IPv4 da máquina.');
+    process.exit(1);
+}
+
+const envContent = `# main
+IP = ${ip}
+
+# host ports
+HOST_PORT=${PORTS.HOST_PORT}
+HOST_WEBSOCKET_PORT=${PORTS.HOST_WEBSOCKET_PORT}
+
+# client port
+CLIENT_PORT=${PORTS.CLIENT_PORT}`;
+
+fs.writeFileSync(path.join(__dirname, './', '.env'), envContent);
+
+const configContent = `window.env = {
+    ip: "${ip}",
+    webSocketPort: "${PORTS.HOST_WEBSOCKET_PORT}"
+};`;
+
+fs.writeFileSync(path.join(__dirname, 'public/js/env/', '.env.js'), configContent);
+
+console.log('.env.js gerado com sucesso!');
+console.log('IP: ', ip);
+console.log('WebSocket Port: ', PORTS.HOST_WEBSOCKET_PORT);
+console.log('Host Port: ', PORTS.HOST_PORT);
+console.log('Client Port: ', PORTS.CLIENT_PORT);
---
+++ generate-options.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+function generateOptionsFile(jsonPath, outputPath) {
+    const jsonRaw = fs.readFileSync(jsonPath, 'utf-8');
+    const jsonData = JSON.parse(jsonRaw);
+
+    const { options = {}, problems = {} } = jsonData;
+
+    const objToString = obj =>
+        Object.entries(obj)
+            .map(([key, val]) => {
+                // se a chave tiver espaço ou hífen, põe aspas simples
+                const keyFormatted = /^[a-zA-Z0-9_]+$/.test(key) ? key : `'${key}'`;
+                return `    ${keyFormatted}: "${val.trim()}"`;
+            })
+            .join(',\n');
+
+    const optionsUseStr = `const optionsUse = {\n${objToString(options)}\n};\n\n`;
+    const optionsProblemsStr = `const optionsProblems = {\n${objToString(problems)}\n};\n\n`;
+    const exportStr = 'export { optionsUse, optionsProblems };';
+
+    const arquivoFinal = optionsUseStr + optionsProblemsStr + exportStr;
+
+    fs.writeFileSync(outputPath, arquivoFinal, 'utf-8');
+    console.log(`Arquivo ${outputPath} criado com sucesso!`);
+}
+
+generateOptionsFile(
+    path.resolve(__dirname, './options.json'),
+    path.resolve(__dirname, './public/js/scripts/options.js')
+);
