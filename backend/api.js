9c4ed42ec6b96f6a6493fb7607a0d2f78c44430c
Ed <108343328+eded001@users.noreply.github.com>
2025-07-26 16:21:39 -0300

    feat: criei uma api restful

---
+++ backend/api.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: './.env' });
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.API_PORT || 5525;
+
+const sequelize = require('../db/db');
+const UserRoutes = require('./routes/UserRoutes');
+
+sequelize.sync()
+    .then(() => {
+        console.log('Banco sincronizado');
+
+        app.use(cors());
+        app.use(express.json());
+        app.use(UserRoutes);
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+
+        app.get('/health', (req, res) => {
+            res.status(200).json({ status: 'OK' });
+        });
+    })
+    .catch(err => {
+        console.error('Erro ao sincronizar banco: ', err);
+    });
