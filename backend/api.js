09438630f414821f9dc25cf88b9b95d7c505a758
Ed <108343328+eded001@users.noreply.github.com>
2025-07-29 13:36:52 -0300

    feat: função de criar admin

--- backend/api.js
+++ backend/api.js
@@ -4,9 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./utils/conn');
 const UserRoutes = require('./routes/UserRoutes');
+const { initAdminUser } = require('./models/initAdminUser');
 
 const app = express();
-const PORT = process.env.API_PORT || 5515;
+const PORT = process.env.API_PORT;
 const IP = process.env.IP;
 
 app.use(cors({
@@ -15,19 +16,24 @@ app.use(cors({
 
 app.use(express.json());
 
+// rotas de usuário
 app.use(UserRoutes);
 
-app.get('/health', (req, res) => {
+// health check
+app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
 });
 
 sequelize.sync()
     .then(() => {
         console.log('Banco sincronizado');
+
         app.listen(PORT, () => {
             console.log(`API rodando em http://${IP}:${PORT}`);
         });
+
+        initAdminUser();
     })
     .catch(err => {
-        console.error('Erro ao sincronizar banco:', err);
+        console.error('Erro ao sincronizar banco: ', err);
     });
