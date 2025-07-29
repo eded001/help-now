126757d24f155df768539467cec353408ccbfb21
Ed <108343328+eded001@users.noreply.github.com>
2025-07-28 23:39:23 -0300

    feat: funções de logout e check

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -38,5 +38,25 @@ module.exports = {
             console.error('Erro no login: ', error);
             res.status(500).json({ error: error.message });
         }
+    },
+
+    async authCheck(req, res) {
+        if (req.session.user) {
+            return res.status(200).json({ authenticated: true, user: req.session.user });
+        } else {
+            return res.status(401).json({ authenticated: false });
+        }
+    },
+
+    async logout(req, res) {
+        req.session.destroy(err => {
+            if (err) {
+                console.error('Erro ao encerrar a sessão: ', err);
+                return res.status(500).json({ error: 'Erro ao fazer logout' });
+            }
+
+            res.clearCookie('connect.sid');
+            return res.status(200).json({ message: 'Logout realizado com sucesso' });
+        });
     }
 };
