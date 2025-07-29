b4c6933ebcb5e631643f3ac3a6db0d8122064f7e
Ed <108343328+eded001@users.noreply.github.com>
2025-07-28 23:41:12 -0300

    feat: separei a api do servidor

--- backend/api.js
+++ backend/api.js
@@ -2,28 +2,32 @@ require('dotenv').config({ path: './.env' });
 
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const PORT = process.env.API_PORT || 5525;
-
 const sequelize = require('./utils/conn');
 const UserRoutes = require('./routes/UserRoutes');
+
+const app = express();
+const PORT = process.env.API_PORT || 5515;
+const IP = process.env.IP;
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(UserRoutes);
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK' });
+});
 
 sequelize.sync()
     .then(() => {
         console.log('Banco sincronizado');
-
-        app.use(cors());
-        app.use(express.json());
-        app.use(UserRoutes);
-
         app.listen(PORT, () => {
-            console.log(`Servidor rodando na porta ${PORT}`);
-        });
-
-        app.get('/health', (req, res) => {
-            res.status(200).json({ status: 'OK' });
+            console.log(`API rodando em http://${IP}:${PORT}`);
         });
     })
     .catch(err => {
-        console.error('Erro ao sincronizar banco: ', err);
+        console.error('Erro ao sincronizar banco:', err);
     });
