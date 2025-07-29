7c8c56e9aa9efcae1e2cc292af3130af5d8dba57
Ed <108343328+eded001@users.noreply.github.com>
2025-07-28 23:40:11 -0300

    feat: rotas com middlewares

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const UserController = require('../controllers/UserController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/create', UserController.create);
 router.post('/login', UserController.login);
+router.get('/auth-check', authMiddleware, UserController.authCheck);
+router.post('/logout', authMiddleware, UserController.logout);
 
 module.exports = router;
