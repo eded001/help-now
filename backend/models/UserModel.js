a1a1a9b75a7e4adbe9ad79cec420415bdbdf145c
Ed <108343328+eded001@users.noreply.github.com>
2025-07-29 13:37:26 -0300

    feat: adicionei novos campos

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -16,6 +16,16 @@ const User = sequelize.define('User', {
         allowNull: false,
         unique: true,
     },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    type_account: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+        defaultValue: 'client',
+    }
 }, {
     tableName: 'users',
     timestamps: false, // se quiser createdAt/updatedAt, muda para true
