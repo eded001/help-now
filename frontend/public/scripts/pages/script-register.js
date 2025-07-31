5a4a6d05bb3ae2e38e9003c3fe0a9264d953b89b
Ed <108343328+eded001@users.noreply.github.com>
2025-07-31 00:38:53 -0300

    feat: mudei os arquivos para uma pasta separada pro frontend

---
+++ frontend/public/scripts/pages/script-register.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('form');
+const usernameInput = document.querySelector("#username-user");
+const idUserInput = document.querySelector("#id-user");
+
+const { ip, port } = window.env;
+
+const url = `http://${ip}:${port}`;
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const username = usernameInput.value.trim();
+    const idUser = idUserInput.value.trim();
+
+    if (username === "" || idUser === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    fetch(url + '/health')
+        .then(response => {
+            if (!response.ok) throw new Error("Erro ao conectar com o servidor.");
+            return response.json();
+        })
+        .then(data => {
+            if (data.error) throw new Error(data.error);
+            if (data.status === 'OK') {
+                return fetch(url + '/api/user/create', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        name: username,
+                        username: idUser,
+                    }),
+                });
+            } else {
+                throw new Error('Servidor indisponível');
+            }
+        })
+        .then(async res => {
+            if (!res.ok) {
+                const data = await res.json();
+                throw new Error(data.error || `Erro ${res.status}: ${res.statusText}`);
+            }
+            return res.json();
+        })
+        .then(data => {
+            alert(`Usuário "${data.username || data.name}" criado com sucesso!`);
+            window.location.href = '/';
+        })
+        .catch(error => {
+            alert(`Erro ao criar usuário: ${error.message}`);
+        });
+});
