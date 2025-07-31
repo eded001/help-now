afddaa2a13ad44c0a088d393ea052478677aa879
Ed <108343328+eded001@users.noreply.github.com>
2025-07-31 00:24:55 -0300

    style: movi as pastas

--- backend/middlewares/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-function adminMiddleware(req, res, next) {
-    if (req.session && req.session.user && req.session.user.isAdmin) {
-        return next();
-    }
-    res.status(403).json({ error: 'Acesso negado: Admins apenas' });
-}
-
-module.exports = adminMiddleware;
