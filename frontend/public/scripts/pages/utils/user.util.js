28b8df7f03af5b9737a311d69f00d9a6de2f65ff
Ed <108343328+eded001@users.noreply.github.com>
2025-08-01 16:01:56 -0300

    feat: modularizei o cadastor e o login do usuário

---
+++ frontend/public/scripts/pages/utils/user.util.js
@@ -0,0 +1,21 @@
+import { request } from "../helpers/api-request.helper.js";
+
+async function register({ username, name, password }) {
+    try {
+        return await request('users/register', 'POST', { username, name, password });
+    } catch (error) {
+        console.error('Erro no registro:', error);
+        throw error;
+    }
+}
+
+async function login({ username, password }) {
+    try {
+        return await request('users/login', 'POST', { username, password });
+    } catch (error) {
+        console.error('Erro no login:', error);
+        throw error;
+    }
+}
+
+export { register, login };
