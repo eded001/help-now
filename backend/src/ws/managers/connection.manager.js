5b3db3c9c6289fd09e1daec97c4c23301c9738c8
Ed <108343328+eded001@users.noreply.github.com>
2025-09-02 14:23:36 -0300

    refactor: mudei o esquema de set de users

--- backend/src/ws/managers/connection.manager.js
+++ backend/src/ws/managers/connection.manager.js
@@ -1,25 +1,29 @@
 const clients = new Map();   // clientId -> socket
 const supports = new Map();  // supportId -> socket
 
-function registerConnection(socket, type, id, username) {
+function registerConnection(socket, type, username) {
     const connectionData = { socket, username };
 
     if (type === 'client') {
-        clients.set(id, connectionData);
+        clients.set(username, connectionData);
     } else if (type === 'support') {
-        supports.set(id, connectionData);
+        supports.set(username, connectionData);
     }
 }
 
 function removeConnection(socket) {
-    if (socket.userType === 'client') clients.delete(socket.userId);
-    if (socket.userType === 'support') supports.delete(socket.userId);
+    if (socket.userType === 'client') clients.delete(socket.username);
+    if (socket.userType === 'support') supports.delete(socket.username);
 }
 
 function broadcastToSupports(message) {
     const msg = JSON.stringify(message);
-    supports.forEach(socket => {
-        if (socket.readyState === WebSocket.OPEN) socket.send(msg);
+    supports.forEach(userSockets => {
+        userSockets.forEach(socket => {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(msg);
+            }
+        });
     });
 }
 
